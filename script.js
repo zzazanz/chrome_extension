38a0f986694f74c34be6cb763316b36c995c7025
zzazan123 <67254656+zzazan123@users.noreply.github.com>
2021-10-11 18:36:19 +0900

    팝업

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
 window.addEventListener('load', () => {
-    // const problem1 = Math.floor(Math.random() * 100);
-    // const problem2 = Math.floor(Math.random() * 100);
-    // document.querySelector("#problem_input").value = "";
-    // document.querySelector("#another_button").hidden = true;
-    // document.querySelector("h1").innerText = problem1 + " + " + problem2;
-    // document.querySelector("#enter_button").addEventListener("click", () => {
-    //     if (parseInt(document.querySelector("#problem_input").value) == problem1 + problem2) {
-    //         alert("right answer!");
-    //         document.querySelector("#another_button").hidden = false;
-    //     } else {
-    //         alert("wrong answer!");
-    //         alert("try again!");
-    //     }
-    // });
-    // document.querySelector("#another_button").addEventListener("click", () => {
-    //     const problem1 = Math.floor(Math.random() * 100);
-    //     const problem2 = Math.floor(Math.random() * 100);
-    //     document.querySelector("#problem_input").value = "";
-    //     document.querySelector("#another_button").hidden = true;
-    //     document.querySelector("h1").innerText = problem1 + " + " + problem2;
-    //     document.querySelector("#enter_button").addEventListener("click", () => {
-    //         if (parseInt(document.querySelector("#problem_input").value) == problem1 + problem2) {
-    //             alert("right answer!");
-    //             document.querySelector("#another_button").hidden = false;
-    //         } else {
-    //             alert("wrong answer!");
-    //             alert("try again!");
-    //         }
-    //     });
-    // });
     document.querySelector("button").addEventListener("click", () => {
-        alert(document.location)
+        alert("Click!");
         
     });
 });
